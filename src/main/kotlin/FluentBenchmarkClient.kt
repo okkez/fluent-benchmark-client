7d623c48c5ef3242b879d643b545067489b3d6c2
Kenji Okimoto <okimoto@clear-code.com>
2018-01-10 16:49:43 +0900

    Support buffer related options

--- src/main/kotlin/FluentBenchmarkClient.kt
+++ src/main/kotlin/FluentBenchmarkClient.kt
@@ -4,6 +4,8 @@ import org.komamitsu.fluency.Fluency
 import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
+import sizeToInt
+import sizeToLong
 
 @Command(name = "FluentdBenchmarkClient", version = ["1.0.0"], description = ["Benchmark client for Fluentd"])
 class FluentBenchmarkClient: Runnable {
@@ -35,6 +37,15 @@ class FluentBenchmarkClient: Runnable {
     @Option(names = ["--require-ack-response"], description = ["Change the protocol to at-least-once (default: false)"])
     private var requireAckResponse: Boolean = false
 
+    // Buffer options
+    @Option(names = ["--buffer-chunk-initial-size"], paramLabel = "SIZE", description = [""])
+    private var bufferChunkInitialSize: String? = null
+
+    @Option(names = ["--buffer-chunk-retention-size"], paramLabel = "SIZE", description = [""])
+    private var bufferChunkRetentionSize: String? = null
+
+    @Option(names = ["--max-buffer-size"], paramLabel = "SIZE", description = ["The max buffer size"])
+    private var maxBufferSize: String? = null
 
     // Load options
     @Option(names = ["--n-events"], paramLabel = "N", description = ["Emit N events"])
@@ -58,6 +69,17 @@ class FluentBenchmarkClient: Runnable {
     override fun run() {
         var conf: Fluency.Config = Fluency.Config()
         conf.isAckResponseMode = requireAckResponse
+
+        if (!bufferChunkInitialSize.isNullOrEmpty()) {
+            conf.bufferChunkInitialSize = sizeToInt(bufferChunkInitialSize!!)
+        }
+        if (!bufferChunkRetentionSize.isNullOrEmpty()) {
+            conf.bufferChunkRetentionSize = sizeToInt(bufferChunkRetentionSize!!)
+        }
+        if (!maxBufferSize.isNullOrEmpty()) {
+            conf.maxBufferSize = sizeToLong(maxBufferSize!!)
+        }
+
         var fluency: Fluency = Fluency.defaultFluency(host, port, conf)
         println("Run!")
         var client = BenchmarkClient(fluency)
