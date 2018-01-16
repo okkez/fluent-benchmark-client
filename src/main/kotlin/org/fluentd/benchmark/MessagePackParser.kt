3a61ad87009de78fba1f05b2b73aef6441ddbb83
Kenji Okimoto <okimoto@clear-code.com>
2018-01-16 18:16:42 +0900

    Add MessagePackParser

---
+++ src/main/kotlin/org/fluentd/benchmark/MessagePackParser.kt
@@ -0,0 +1,9 @@
+package org.fluentd.benchmark
+
+import java.nio.ByteBuffer
+
+class MessagePackParser: Parser<ByteBuffer> {
+    override fun parse(text: String, block: (ByteBuffer) -> Unit) {
+        block(ByteBuffer.wrap(text.toByteArray()))
+    }
+}
