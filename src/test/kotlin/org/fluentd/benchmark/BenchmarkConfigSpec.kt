901cbde1554b43ed8efed50019be788b82ac8add
okkez <okkez000@gmail.com>
2018-01-18 23:25:11 +0900

    Use JUnit5 for assertions

--- src/test/kotlin/org/fluentd/benchmark/BenchmarkConfigSpec.kt
+++ src/test/kotlin/org/fluentd/benchmark/BenchmarkConfigSpec.kt
@@ -4,7 +4,8 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.msgpack.core.MessagePack
 
 object BenchmarkConfigSpec: Spek({
--- src/test/kotlin/org/fluentd/benchmark/LTSVParserSpec.kt
+++ src/test/kotlin/org/fluentd/benchmark/LTSVParserSpec.kt
@@ -4,7 +4,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.msgpack.core.MessagePack
 
 object LTSVParserSpec: Spek({
