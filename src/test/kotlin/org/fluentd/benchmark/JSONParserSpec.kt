f15dace1edb952e958ffcf450148f01354a5f8aa
Kenji Okimoto <okimoto@clear-code.com>
2018-01-18 17:57:22 +0900

    Add test for LTSVParser and JSONParser

---
+++ src/test/kotlin/org/fluentd/benchmark/JSONParserSpec.kt
@@ -0,0 +1,26 @@
+package org.fluentd.benchmark
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.Assert.assertEquals
+import org.msgpack.core.MessagePack
+
+object JSONParserSpec: Spek({
+    given("a JSON parser") {
+        on("parse JSON") {
+            val parser = JSONParser()
+            it("returns ByteArray represents map") {
+                parser.parse("""{ "key1": "value1", "key2": "value2" }""") {
+                    val unpacker = MessagePack.newDefaultUnpacker(it)
+                    assertEquals(2, unpacker.unpackMapHeader())
+                    assertEquals("key1", unpacker.unpackString())
+                    assertEquals("value1", unpacker.unpackString())
+                    assertEquals("key2", unpacker.unpackString())
+                    assertEquals("value2", unpacker.unpackString())
+                }
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/org/fluentd/benchmark/LTSVParserSpec.kt
@@ -0,0 +1,26 @@
+package org.fluentd.benchmark
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.Assert.assertEquals
+import org.msgpack.core.MessagePack
+
+object LTSVParserSpec: Spek({
+    given("a LTSV parser") {
+        on("parse LTSV text") {
+            val parser = LTSVParser()
+            it("returns ByteArray that represents map") {
+                parser.parse("key1:value1\tkey2:value2") {
+                    val unpacker = MessagePack.newDefaultUnpacker(it)
+                    assertEquals(2, unpacker.unpackMapHeader())
+                    assertEquals("key1", unpacker.unpackString())
+                    assertEquals("value1", unpacker.unpackString())
+                    assertEquals("key2", unpacker.unpackString())
+                    assertEquals("value2", unpacker.unpackString())
+                }
+            }
+        }
+    }
+})
