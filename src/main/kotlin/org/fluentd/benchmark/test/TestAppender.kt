11586d6807e2bbf14b35ce482eab2d254a3a865b
okkez <okkez000@gmail.com>
2018-01-19 21:08:40 +0900

    Use log4j2 instead of legacy log4j

--- src/main/kotlin/org/fluentd/benchmark/test/TestAppender.kt
+++ src/main/kotlin/org/fluentd/benchmark/test/TestAppender.kt
@@ -1,25 +1,41 @@
 package org.fluentd.benchmark.test
 
-import org.apache.log4j.AppenderSkeleton
-import org.apache.log4j.spi.LoggingEvent
+import org.apache.logging.log4j.core.Filter
+import org.apache.logging.log4j.core.Layout
+import org.apache.logging.log4j.core.LogEvent
+import org.apache.logging.log4j.core.appender.AbstractAppender
+import org.apache.logging.log4j.core.config.plugins.Plugin
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute
+import org.apache.logging.log4j.core.config.plugins.PluginElement
+import org.apache.logging.log4j.core.config.plugins.PluginFactory
+import java.io.Serializable
 
-class TestAppender: AppenderSkeleton() {
+@Plugin(name = "TestAppender", category = "Core", elementType = "appender", printObject = true)
+class TestAppender : AbstractAppender {
+    constructor(name: String?,
+                filter: Filter?,
+                layout: Layout<out Serializable>?) : super(name, filter, layout)
+    constructor(name: String?,
+                filter: Filter?,
+                layout: Layout<out Serializable>?,
+                ignoreExceptions: Boolean) : super(name, filter, layout, ignoreExceptions)
 
     companion object {
-        var events = mutableListOf<LoggingEvent>()
-    }
+        var events = mutableListOf<LogEvent>()
 
-    override fun requiresLayout(): Boolean {
-        return false
-    }
-
-    override fun append(event: LoggingEvent?) {
-        if (event != null) {
-            events.add(event)
+        @JvmStatic
+        @PluginFactory
+        fun createAppender(@PluginAttribute("name") name: String,
+                           @PluginAttribute("ignoreExceptions") ignoreExceptions: Boolean,
+                           @PluginElement("Layout") layout: Layout<out Serializable>?,
+                           @PluginElement("Filters") filter: Filter?): TestAppender {
+            return TestAppender(name, filter, layout)
         }
     }
 
-    override fun close() {
-        // Do nothing
+    override fun append(event: LogEvent?) {
+        if (event != null) {
+            events.add(event)
+        }
     }
 }
