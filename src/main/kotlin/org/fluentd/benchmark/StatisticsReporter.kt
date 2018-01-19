f384db078c3a9b6e0bb36a92151dc001db34e28d
okkez <okkez000@gmail.com>
2018-01-19 16:17:52 +0900

    Use logger in StatisticsReporter

--- src/main/kotlin/org/fluentd/benchmark/StatisticsReporter.kt
+++ src/main/kotlin/org/fluentd/benchmark/StatisticsReporter.kt
@@ -1,8 +1,15 @@
 package org.fluentd.benchmark
 
+import org.slf4j.LoggerFactory
+
 class StatisticsReporter(private val statistics: Statistics) {
+    companion object {
+        val log = LoggerFactory.getLogger(this::class.java.enclosingClass)!!
+    }
+
     fun report() {
-        println("""
+        log.info("""
+
             totalEvents: ${statistics.nEvents()}
             totalElapsed(sec): ${statistics.totalElapsedTime()}
             average(events/sec): ${statistics.average(statistics.totalElapsedTime())}
