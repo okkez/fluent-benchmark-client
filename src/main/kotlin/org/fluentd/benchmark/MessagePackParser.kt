2c89c3781b873136df8cc1bc646b11b50b8d46c5
Kenji Okimoto <okimoto@clear-code.com>
2018-01-19 09:03:59 +0900

    Remove MessagePackParser

--- src/main/kotlin/org/fluentd/benchmark/MessagePackParser.kt
+++
@@ -1,7 +0,0 @@
-package org.fluentd.benchmark
-
-class MessagePackParser: Parser<ByteArray> {
-    override fun parse(text: String, block: (ByteArray) -> Unit) {
-        block(text.trim().toByteArray())
-    }
-}
