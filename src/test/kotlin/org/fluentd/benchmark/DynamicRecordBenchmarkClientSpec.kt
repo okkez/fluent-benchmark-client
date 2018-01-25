3fd5c45a150c17cc16033f4907e3b04850f48ae5
Kenji Okimoto <okimoto@clear-code.com>
2018-01-25 09:58:22 +0900

    Fix test duration
    Because interval is in seconds, and we can process an event in a
    second running in interval mode.

--- src/test/kotlin/org/fluentd/benchmark/DynamicRecordBenchmarkClientSpec.kt
+++ src/test/kotlin/org/fluentd/benchmark/DynamicRecordBenchmarkClientSpec.kt
@@ -65,14 +65,14 @@ object DynamicRecordBenchmarkClientSpec: Spek({
                 assertEquals(10000L, server.processedEvents())
             }
         }
-        on("runs in fixed interval mode, processes 10000 events") {
+        on("runs in fixed interval mode, processes 3 events") {
             val fluencyConfig = Fluency.Config()
             fluencyConfig.flushIntervalMillis = 100
             val benchmarkConfig = BenchmarkConfig.create {
                 tag = "test.tag"
                 timestampType = BenchmarkClient.TimestampType.EventTime
-                nEvents = 10000
-                interval = 100 // msec
+                nEvents = 3
+                interval = 1 // sec
                 period = null
                 recordKey = "message"
                 recordValue = "This is test message."
@@ -84,12 +84,12 @@ object DynamicRecordBenchmarkClientSpec: Spek({
             }
             val client = DynamicRecordBenchmarkClient("127.0.0.1", 24224, fluencyConfig, benchmarkConfig)
             val server = TestServer()
-            it("processes 10000 events") {
-                server.run(10000L) {
+            it("processes 3 events") {
+                server.run(3L) {
                     client.run()
                 }
-                assertEquals(10000L, client.eventCounter.get())
-                assertEquals(10000L, server.processedEvents())
+                assertEquals(3L, client.eventCounter.get())
+                assertEquals(3L, server.processedEvents())
             }
         }
     }
