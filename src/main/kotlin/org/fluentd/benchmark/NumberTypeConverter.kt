186566b832912f6697696d95b58bc5a98199a62f
Kenji Okimoto <okimoto@clear-code.com>
2018-02-02 14:47:26 +0900

    Add NumberTypeConverter to specify huge number of events

---
+++ src/main/kotlin/org/fluentd/benchmark/NumberTypeConverter.kt
@@ -0,0 +1,25 @@
+package org.fluentd.benchmark
+
+import picocli.CommandLine
+
+class NumberTypeConverter: CommandLine.ITypeConverter<Number> {
+    override fun convert(value: String?): Number {
+        if (value.isNullOrEmpty()) {
+            return 0
+        }
+        val pattern = Regex("""\A(\d+)([kKmMgG])\z""")
+        val m = pattern.matchEntire(value!!)
+        if (m != null) {
+            val digit = m.groupValues[1].toLong()
+            val unit: Long = when (m.groupValues[2]) {
+                "k", "K" -> 1000
+                "m", "M" -> 1000 * 1000
+                "g", "G" -> 1000 * 1000 * 1000
+                else -> 1
+            }
+            return digit * unit
+        }
+
+        return 0
+    }
+}
---
+++ src/test/kotlin/org/fluentd/benchmark/NumberTypeConverterSpec.kt
@@ -0,0 +1,27 @@
+package org.fluentd.benchmark
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import org.junit.jupiter.api.Assertions.assertEquals
+
+object NumberTypeConverterSpec: Spek({
+    given("a size converter") {
+        on("convert %s",
+                data("1k", expected = 1000L),
+                data("5K", expected = 5000L),
+                data("1m", expected = 1000000L),
+                data("5M", expected = 5000000L),
+                data("1g", expected = 1000000000L),
+                data("5G", expected = 5000000000L)
+        ) { value: String, expected: Number ->
+            val converter = NumberTypeConverter()
+            it("returns $expected") {
+                assertEquals(expected, converter.convert(value))
+            }
+        }
+    }
+})
