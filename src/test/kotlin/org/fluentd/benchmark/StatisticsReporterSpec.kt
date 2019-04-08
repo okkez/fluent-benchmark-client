f52fa3274f7306d3d13e2f3feabce7d18630ed46
okkez <okkez000@gmail.com>
2019-04-08 12:35:52 +0900

    test: Fix assertion

--- src/test/kotlin/org/fluentd/benchmark/StatisticsReporterSpec.kt
+++ src/test/kotlin/org/fluentd/benchmark/StatisticsReporterSpec.kt
@@ -1,5 +1,6 @@
 package org.fluentd.benchmark
 
+import org.apache.logging.log4j.core.LogEvent
 import org.fluentd.benchmark.test.TestAppender
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -12,20 +13,17 @@ object StatisticsReporterSpec: Spek({
             TestAppender.events.clear()
         }
         context("report") {
-            val statistics = Statistics()
-            statistics.set(1000)
-            TimeUnit.MILLISECONDS.sleep(200L)
-            statistics.finish()
-            val reporter = StatisticsReporter(statistics)
             it("displays statistics") {
+                val statistics = Statistics()
+                statistics.set(1000)
+                TimeUnit.MILLISECONDS.sleep(200L)
+                statistics.finish()
+                val reporter = StatisticsReporter(statistics)
                 reporter.report()
-                val event = TestAppender.events.first()
+                val event: LogEvent = TestAppender.events.first()
                 assertEquals(1, TestAppender.events.size)
                 assertEquals(3, event.message.parameters.size)
-                val (nEvents, elapsed, average) = event.message.parameters
-                assertEquals(1000L, nEvents)
-                assertEquals(0.2f, elapsed as Float, 0.01f)
-                assertEquals(5000.0f, average as Float, 30f)
+                assertEquals("\ntotalEvents: 1000\ntotalElapsed(sec): 0.2\naverage(events/sec): 5000.0", event.message.formattedMessage)
             }
         }
     }
